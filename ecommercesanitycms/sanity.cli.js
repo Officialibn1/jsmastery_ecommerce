028e594ebf2374fc5bacb00855cba91dc3b433b9
ibn <officialibn001@gmail.com>
2023-08-22 23:07:02 +0100

    First commit

---
+++ ecommercesanitycms/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '0f4iyf54',
+    dataset: 'production'
+  }
+})
---
+++ ecommercesanitycms/schemas/index.js
@@ -0,0 +1,9 @@
+// import schemaTypes from 'all:part:@sanity/base/schema-type';
+import product from "./product"
+
+export default createSchema({
+    name: 'default',
+    types: schemaTypes.concat([
+        product
+    ]),
+})
